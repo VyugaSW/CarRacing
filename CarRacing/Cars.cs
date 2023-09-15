2270ca356229e93b3b73a2733e58c1fba1260a50
Vyuga Snow <Wolf-snow00@mail.ru>
2023-09-15 21:06:43 +0300

    Добавьте файлы проекта.

---
+++ CarRacing/Cars.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRacing
+{
+
+
+    internal abstract class Car : IComparable
+    {
+        protected const int DistanceForFinish = 10000;
+
+        public abstract int MaxSpeed { get; }
+        public abstract int MinSpeed { get; }
+        public int Position { get => _position; set => _position = value; }
+
+
+        
+
+        protected int _position = 0;
+        protected int _speed = 0;
+        protected int _distance = 0;
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Car car) return _distance.CompareTo(car._distance);
+            else throw new ArgumentException();
+        }
+
+        public override string ToString()
+        {
+            return $"Type of car: {this.GetType().ToString().Replace("CarRacing.", "")}\n" +
+                $"Current speed: {_speed}\nPosition: {_position}\n" +
+                $"Distance: {_distance}";
+        }
+        public int UpdateDistancePerOneSecond()
+        {
+            if (_distance < DistanceForFinish)
+                _distance += _speed;
+            else
+                return Finish();
+            return 0;
+        }
+        public void Move()
+        {
+            Random random = new Random();
+            _speed = random.Next(MinSpeed, MaxSpeed);
+        }
+        public int Finish()
+        {
+            return -1;
+        }
+
+    }
+
+ 
+
+    internal class SportCar : Car
+    {
+        static int _maxSpeed = 200;
+        static int _minSpeed = 90;
+
+
+        public override int MaxSpeed { get => _maxSpeed; }
+        public override int MinSpeed { get => _minSpeed; }
+
+        public SportCar() : base() { }
+
+    }
+
+    internal class CargoCar : Car
+    {
+        static int _maxSpeed = 135;
+        static int _minSpeed = 85;
+
+        public override int MaxSpeed { get => _maxSpeed; }
+        public override int MinSpeed { get => _minSpeed; }
+
+        public CargoCar() : base() { }
+
+
+
+
+    }
+
+    internal class LightCar : Car
+    {
+        static int _maxSpeed = 160;
+        static int _minSpeed = 120;
+
+        public override int MaxSpeed { get => _maxSpeed; }
+        public override int MinSpeed { get => _minSpeed; }
+
+        public LightCar() : base() { }
+    }
+
+    internal class Bus : Car
+    {
+        static int _maxSpeed = 120;
+        static int _minSpeed = 100;
+
+        public override int MaxSpeed { get => _maxSpeed; }
+        public override int MinSpeed { get => _minSpeed; }
+
+        public Bus() : base() { }
+    }
+
+
+}
---
+++ CarRacing/Game.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRacing
+{
+
+    public delegate void CarMovingDelegate();
+    public delegate int CarUpdateDistanceDelegate();
+
+
+    internal static class GameDisplay
+    {
+        static string sportCarImage = " _    _________        \n" +
+                                      " |___/         \\_____ \n" +
+                                      "  |___()________()___/ ";
+
+        static string lightCarImage = "     ________          \n" +
+                                      " ___/       \\____     \n" +
+                                      " ^===()========()=^    ";
+
+        static string CargoCarImage = "  ____________         \n" +
+                                      " /           \\_____   \n" +
+                                      " |___( )_____( )__||   ";
+
+        static string BusImage =      " _____=_________=___   \n" +
+                                      " |                 ||  \n" +
+                                      " |_()_()_______()__|/  ";
+
+        static private string GetStringPosition(Car car)
+        {
+            switch (car.Position)
+            {
+                case 0:
+                    return "START";
+                case 1:
+                    return "FIRST";
+                case 2:
+                    return "SECOND";
+                case 3:
+                    return "THIRD";
+            }
+
+            throw new Exception();
+        }
+        static private string CarImagine(Car car)
+        {
+            if (car is SportCar)
+                return sportCarImage;
+
+            else if (car is CargoCar)
+                return CargoCarImage;
+
+            else if (car is LightCar)
+                return lightCarImage;
+
+            else
+                return BusImage;
+
+            throw new Exception();
+        }
+        static public void DisplayPositions(List<Car> cars)
+        {
+            Console.WriteLine("(Press Enter to continue the racing)\n");
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"The {GetStringPosition(car)} car:\n" + car);
+                Console.WriteLine(CarImagine(car));
+                Console.WriteLine("--------------------------");
+            }
+
+        }
+        static public void DisplayMainMenu()
+        {
+            Console.WriteLine("!!!CAR RACING!!!");
+            Console.WriteLine("1 - One car");
+            Console.WriteLine("2 - Two cars");
+            Console.WriteLine("3 - Three cars");
+        }
+        static public void DisplayChoiceMenu()
+        {
+            Console.WriteLine("!!!CHOOSE YOUR CAR!!!");
+            Console.WriteLine("1 - Sport car");
+            Console.WriteLine("2 - Light car");
+            Console.WriteLine("3 - Cargo car");
+            Console.WriteLine("4 - Bus");
+        }
+        static public void DisplayWonCar(Car car)
+        {
+            Console.Clear();
+            Console.WriteLine("Car:\n" + car + '\n' + CarImagine(car) + '\n' + "\nHAS WON!!!");
+        }
+
+    }
+
+    internal class Game
+    {
+        List<Car> cars;
+        CarMovingDelegate carMoving;
+        CarUpdateDistanceDelegate carUpdateDistance;
+
+        public Game()
+        {
+            cars = new List<Car>();
+        }
+        
+        public void AddCar(Car car)
+        {
+            if (cars.Count <= 3)
+                cars.Add(car);
+            else
+                throw new OutOfMemoryException();
+        }
+        private void AddFunctionInDelegate()
+        {
+            for(int i = cars.Count()-1; i > -1 ; i--)
+            {
+                carMoving += cars[i].Move;
+                carUpdateDistance += cars[i].UpdateDistancePerOneSecond;
+            }
+        }
+        private void RemoveFunctionFromDelegate()
+        {
+            foreach (Car car in cars)
+            {
+                carMoving -= car.Move;
+                carUpdateDistance -= car.UpdateDistancePerOneSecond;
+            }
+        }
+        private void UpdateCarsPositions()
+        {
+            if (cars.Count == 0)
+                throw new Exception();
+
+            cars.Sort();
+            cars.Reverse();
+            for (int i = 0; i < cars.Count; i++)
+                cars[i].Position = i + 1;
+        }
+        private void ProccessOfRacing()
+        {
+            while (carUpdateDistance() != -1)
+            {
+                Console.Clear();
+
+                carMoving();
+                UpdateCarsPositions();
+                GameDisplay.DisplayPositions(cars);
+
+
+                RemoveFunctionFromDelegate();
+                AddFunctionInDelegate();
+
+                Console.ReadKey();
+            }
+        }
+        private void EndOfRacing()
+        {
+            GameDisplay.DisplayWonCar(cars[0]);
+        }
+
+        private int ChoiceMainMenu()
+        {
+            GameDisplay.DisplayMainMenu();
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        private bool AddCarFromChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    AddCar(new SportCar());
+                    break;
+                case 2:
+                    AddCar(new LightCar());
+                    break;
+                case 3:
+                    AddCar(new CargoCar());
+                    break;
+                case 4:
+                    AddCar(new Bus());
+                    break;
+               default:
+                    return false;
+            }
+            return true;
+        }
+        private void ChoiceMenuCarsAdd()
+        {
+            int carsCount = 0;
+            while((carsCount = ChoiceMainMenu()) != 1 && carsCount != 2 && carsCount != 3)
+                Console.Clear();
+
+            while(carsCount != 0)
+            {
+                Console.Clear();
+                GameDisplay.DisplayChoiceMenu();
+                if (AddCarFromChoice(Convert.ToInt32(Console.ReadLine())))
+                    carsCount--;            
+            }
+        }
+        
+
+        public int GameMain()
+        {
+            ChoiceMenuCarsAdd();
+            AddFunctionInDelegate();
+            ProccessOfRacing();
+            EndOfRacing();
+
+            return 1;
+        }
+
+    }
+}
